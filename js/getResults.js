bbd6f05796ce6f58f0f762c0d99c30c9e9ff96b2
Jeffrey Ye <jeffreyye16@gmail.com>
2021-11-25 05:08:06 -0800

    prep for getting results

---
+++ js/getResults.js
@@ -0,0 +1,2 @@
+// nothing to do here yet, just need to save get here
+var test = sessionStorage.getItem('lastName');
--- js/saveLast.js
+++
@@ -1,4 +0,0 @@
-var item = document.getElementById('lastName').value;
-sessionStorage.setItem('lastName', item);
-
-console.log(sessionStorage.getItem('lastName'));
